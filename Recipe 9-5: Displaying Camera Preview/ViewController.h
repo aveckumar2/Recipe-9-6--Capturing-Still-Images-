dca542f74d1b4138a7cac2ff87be2bc25930598c
joe Hoffman <josephleehoffman@gmail.com>
2013-08-22 16:54:06 -0600

    Initial Commit

---
+++ Recipe 9-5: Displaying Camera Preview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Recipe 9-5: Displaying Camera Preview
+//
+//  Created by joseph hoffman on 8/22/13.
+//  Copyright (c) 2013 NSCookbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
